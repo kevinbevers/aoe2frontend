343152a43a5359933e9d156f5404bf83ec0861a0
Dennis Keil <dennis.keil10@gmail.com>
2022-10-23 18:05:08 +0200

    lobbies

--- apps/web/pages/lobby.tsx
+++ apps/web/pages/lobby.tsx
@@ -13,7 +13,7 @@ export function initConnection(onConnected: () => void, onLobbies: (_lobbies: an
     return new Promise(resolve => {
         console.log('ENVIRONMENT', process.env.NEXT_PUBLIC_ENVIRONMENT);
 
-        const client = new w3cwebsocket(`ws://aoe2backend-socket.deno.dev/`);
+        const client = new w3cwebsocket(`wss://aoe2backend-socket.deno.dev/`);
 
         client.onopen = () => {
             console.log('WebSocket Client Connected');
